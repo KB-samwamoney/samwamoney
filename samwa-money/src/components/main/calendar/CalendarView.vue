c5e483681ba071a75496ba82083e529d91e91453
jionii <athena020219@naver.com>
2025-04-09 12:59:51 +0900

    feat: 월별 컴포넌트 구현

--- samwa-money/src/components/main/calendar/CalendarView.vue
+++ samwa-money/src/components/main/calendar/CalendarView.vue
@@ -9,9 +9,9 @@
       <CalendarBody />
     </div>
 
-    <div v-else class="calendar-content">
-      <p>월별 요약이 여기에 표시됩니다.</p>
-    </div>
+    <MonthlyBody
+      v-if="mode === 'monthly'"
+    />
   </div>
 </template>
 
@@ -19,15 +19,15 @@
 import { ref } from 'vue'
 import CalendarHeader from './CalendarHeader.vue'
 import CalendarBody from './CalendarBody.vue'
+import MonthlyBody from './MonthlyBody.vue'
 
 const mode = ref('calendar')
 </script>
 
 <style scoped>
 .calendar-view {
-  background: rgb(255, 234, 234);
   width: 100%;
-  height: 100%; /* 화면 전체 높이 설정 */
+  height: 100%;
   display: flex;
   flex-direction: column;
   box-sizing: border-box;
