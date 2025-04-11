35b54ea2252a78531e19e87477ffe0b7a38704f0
M6F\student <lyw1212@gmail.com>
2025-04-11 14:38:36 +0900

    css

--- samwa-money/src/components/main/calendar/MonthlyBody.vue
+++ samwa-money/src/components/main/calendar/MonthlyBody.vue
@@ -1,22 +1,10 @@
 <template>
   <div class="month-list">
-    <div
-      v-for="month in months"
-      :key="month"
-      class="month-block"
-    >
-      <button
-        @click="selectMonth(month)"
-        class="month-button"
-      >
-        {{ month }}월
-      </button>
+    <div v-for="month in months" :key="month" class="month-block">
+      <button @click="selectMonth(month)" class="month-button">{{ month }}월</button>
 
       <!-- 해당 달이 선택되었을 때만 리스트 보여주기 -->
-      <MonthlyList
-        v-if="selectedMonth === month"
-        :month="month"
-      />
+      <MonthlyList v-if="selectedMonth === month" :month="month" />
     </div>
   </div>
 </template>
@@ -49,13 +37,14 @@ function selectMonth(month) {
 .month-button {
   width: 100%;
   padding: 0.75rem 1rem;
-  background-color: #fff;
-  border: 2px solid #eee;
+  background-color: var(--white);
+  border: 2px solid var(--light-gray);
   border-radius: 8px;
   font-size: 16px;
   text-align: left;
   cursor: pointer;
   transition: background-color 0.3s;
+  color: var(--black);
 }
 
 .month-button:hover {
